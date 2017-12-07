943bfe5484c5d841f65604c317919cee3efaa7cd
Unknown <qvin0000@gmail.com>
2017-12-07 21:46:57 +0300

    Fix entitylabel

--- src/Poe/Elements/EntityLabel.cs
+++ src/Poe/Elements/EntityLabel.cs
@@ -2,16 +2,29 @@ namespace PoeHUD.Poe.Elements
 {
     public class EntityLabel : Element
     {
-        public string Text
+        public int Length
         {
             get
             {
                 int LabelLen = M.ReadInt(Address + 0xC28);
                 if (LabelLen <= 0 || LabelLen > 256)
                 {
+                    return 0;
+                }
+                return LabelLen;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                var LabelLen = Length;
+                if (LabelLen <= 0 || LabelLen > 256)
+                {
                     return "";
                 }
-                return LabelLen >= 8 ? M.ReadStringU(M.ReadInt(Address + 0xC18), LabelLen * 2) : M.ReadStringU(Address + 0xC18, LabelLen * 2);
+                return LabelLen >= 8 ? M.ReadStringU(M.ReadLong(Address + 0xC18), LabelLen * 2) : M.ReadStringU(Address + 0xC18, LabelLen * 2);
             }
         }
     }
